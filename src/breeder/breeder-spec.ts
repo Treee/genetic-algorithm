f2466a1c295a9f57361d2d3efdd3a852e6d254d2
Treee <jonathon.lindemann@gmail.com>
2019-06-19 16:04:51 -0600

    first failing test

---
+++ src/breeder/breeder-spec.ts
@@ -0,0 +1,12 @@
+import { Breeder } from './breeder';
+
+describe('Breeder', () => {
+    let breeder: Breeder;
+    beforeEach(() => {
+        breeder = new Breeder();
+    });
+
+    it('has a population of agents', () => {
+
+    });
+});
